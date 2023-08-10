cefcce9f9e6948c9e4836eace8e335bede2102b9
King-Croon <kingofcroons@gmail.com>
2023-08-09 17:18:42 -0700

    whatsmyname refixed

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,12 +1,13 @@
+#include <stdio.h>
+#include <main.h>
 /**
- * _whatsmyname - writes its own name
+ * main: writes its own name
  *
- * @c: character to determine
- *
+ * @argc: argument count
+ * @argv: argument vector
  * Return: 0
  */
-#include <stdio.h>
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
 	return (0);
