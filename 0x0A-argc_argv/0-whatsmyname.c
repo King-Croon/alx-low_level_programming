4104a51b1b165920efe7c335e7e1c579831f4d21
King-Croon <kingofcroons@gmail.com>
2023-08-09 17:00:59 -0700

    whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+/**
+ * _whatsmyname - writes its own name
+ *
+ * @c: character to determine
+ *
+ * Return: 0
+ */
+#include <stdio.h>
+int main(void)
+{
+	printf("%s", argv[0]);
+	return (0);
+}
