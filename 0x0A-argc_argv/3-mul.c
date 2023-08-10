87da8db002b27c7454c890bc6593a1e142588339
King-Croon <kingofcroons@gmail.com>
2023-08-09 18:23:36 -0700

    betty corrected

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "main.h"
+#include <stdlib.h>
 /**
  * main - prints the products of two values
  * @argv: argument value
@@ -10,16 +11,17 @@
 
 int main(int argc, char *argv[])
 {
-	if(argc == 1)
+	if (argc > 2)
 	{
 		int s1 = atoi(argv[1]);
-		int s2 =atoi( argv[2]);
-		printf("%d\n", s1*s2);
+		int s2 = atoi(argv[2]);
 
+		printf("%d\n", s1 * s2);
 		return (0);
 	}
 	else
 	{
 		printf("Error\n");
+
 		return (1);
 	}
