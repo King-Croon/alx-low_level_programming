ff7b4b10e6e0b36d41e5e70144af0826b8cce271
King-Croon <kingofcroons@gmail.com>
2023-08-09 17:32:11 -0700

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+/*
+ * main - prints argument count
+ * @argc: count of arguments
+ * @argv: values of arguments
+ * Return - (0) success always
+ * */
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	printf("%d\n", argc);
+	return (0);
+}
